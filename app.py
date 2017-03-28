d543aa063f90e564edc9834fad0f3a7613b4f1f4
richiverse <richard.fernandez.nyc@gmail.com>
2017-03-28 16:15:09 -0400

    keeping this in commit log, in case

--- app.py
+++ app.py
@@ -1,25 +1,53 @@
 #! /usr/bin/env python2
 """Gdrive/Flask/Zappa"""
 
+from ast import literal_eval
+from functools import partial
+from io import BytesIO
 import json
-from os import environ as env
+from os import environ as env, remove
 from traceback import format_exc
 import urlparse
+from werkzeug.utils import secure_filename
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response, stream_with_context
 import pandas as pd
 from pydrive.auth import GoogleAuth, AuthError
 from pydrive.drive import GoogleDrive
 from pydrive.files import ApiRequestError
 from pydrive.settings import InvalidConfigError
-from six import StringIO
 
 #from utils import string_resource_to_json
 from middleware import list_routes
 
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/tmp/uploads'
+
+ALLOWED_EXTENSIONS = dict(
+    csv='text/csv',
+    doc='application/msword',
+    docx='application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    json='application/json',
+    pdf='application/pdf',
+    png='image/png',
+    ppt='application/vnd.ms-powerpoint',
+    pptx='application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    svg='image/svg+xml',
+    txt='text/plain',
+    xls='application/vnd.ms-excel',
+    xlsx='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+)
+
+PANDAS_FILES = dict(
+        csv=pd.read_csv,
+        xls=pd.read_excel,
+        xlsx=pd.read_excel,
+        json=pd.read_json,
+)
 
 def init_auth():
+    """initialize auth when needed"""
     try:
         gauth = GoogleAuth(
             settings_file='settings-%(stage)s.yaml' % dict(stage=env.get('STAGE'))
@@ -43,15 +71,31 @@ def init_auth():
 
 @app.errorhandler(Exception)
 def exception_handler(error):
-    raise ValueError, format_exc()
+    """Show uncaught exceptions."""
+    raise Exception, format_exc()
 
 @app.route('/gdrive')
 def list_api_routes():
-    """List all endpoints"""
+    """List all endpoints
+
+    Args:
+        None
+
+    Returns:
+        json list of endpoints.
+    """
     return jsonify(list_routes(app))
 
 @app.route('/gdrive/metadata')
 def get_file_metadata():
+    """Get all metadata on a file or folder.
+
+    Args:
+        gdrive url <str>
+
+    Returns:
+        json list of lists.
+    """
     drive = init_auth()
     url = request.args.get('url')
     parsed_id = parse_url(url)
@@ -66,40 +110,63 @@ def parse_url(url):
     url_id = queries['id'][0] if 'id' in queries else path[3]
     return url_id
 
+def file_reader(file_extension, **kwargs):
+    if file_extension in PANDAS_FILES:
+        return partial(PANDAS_FILES[file_extension], **kwargs)
+    else:
+        return partial(open, mode='rb', buffering=10240)
+
+def yield_dataframe(data):
+    """Yield results to fit AWS API Gateway 5MB payload limit."""
+    for i in range(0, data.shape[0], 1024):
+        yield str(data.iloc[i: i + 1024].to_json(orient='records'))
+
+def yield_bytes(data):
+    ONE_MEG
+    while True:
+        data_bytes = data.read(1024)
+        if not data_bytes:
+            break
+        yield data_bytes
+
+def yield_results(data, mimetype):
+    if isinstance(data, pd.DataFrame):
+        return Response(yield_dataframe(data), mimetype='application/json')
+    else:
+        return Response(yield_bytes(data), mimetype=mimetype)
+
 @app.route('/gdrive/read', methods=['GET'])
 def read_file():
     """Given a URL or ID of a URL, return the parent folder id and
-    the csv file contents as json
-    https://drive.google.com/open?id=0ByfAQQm9a-DuNEgyXzFoNktCT1k
+    file contents as json.
+
+    Args: gdrive url <str>
+        **kwargs, any valid kwarg for underlying mimetype for
+        pandas read_csv, read_excel, read_json methods.
+
+    Returns:
+        json list of dicts for csv, xls[x], json
+        OR binary representation of mimetype
     """
     drive = init_auth()
+    #kwargs = request.args.to_dict()
+    #kwargs.pop('url')
+    #kwargs = {str(k): literal_eval(v) for k,v in kwargs.items() if kwargs}
+
     url = request.args.get('url')
     parsed_id = parse_url(url)
     ifile = drive.CreateFile(dict(id=parsed_id))
-    ifile_contents = StringIO(ifile.GetContentString())
-    df = pd.read_csv(ifile_contents)
-    folder_id = ifile['parents'][0]['id']
+    mimetype = ifile['mimeType']
     title = ifile['title']
-    return jsonify(
-        dict(folder_id=folder_id,
-             data=df.to_json(orient='records'),
-             file_name=title,
-        )
-    )
 
-def validate_file_name(file_name, valid_exts=['csv']):
-    if not file_name:
-        raise('no file provided')
+    tmp_file = '/tmp/{}'.format(title)
+    ifile.GetContentFile(tmp_file, mimetype=mimetype)
+    file_extension = ifile['fileExtension']
 
-    folder, ext = file_name.split('.')[:-1][0], file_name.split('.')[-1]
+    data = open(tmp_file, 'rb')
+    remove(tmp_file)
 
-    if not folder:
-        raise('not a valid folder')
-
-    if ext not in valid_exts:
-        raise('Only csv is supported at this time.')
-
-    return dict(file_name=file_name, folder=folder, ext=ext)
+    return yield_results(data, mimetype)
 
 def create_folder(parent_folder_id, folder_name, drive):
     try:
@@ -115,22 +182,6 @@ def create_folder(parent_folder_id, folder_name, drive):
     except:
         pass
     return folder['id']
-
-def create_file(folder_id, file_name, data, drive):
-    try:
-        ifile = drive.CreateFile(dict(
-            title=file_name,
-            parents=[dict(id=folder_id)],
-            mimeType='text/csv'))
-    except:
-        raise("Couldn't create %(file_name)s" % file_name)
-    try:
-        df = pd.read_json(data)
-    except:
-        df = pd.DataFrame(data)
-    ifile.SetContentString(df.to_csv(index=False))
-    ifile.Upload()
-    return ifile['alternateLink']
 
 def list_folder(folder_id, drive):
     _q = {'q': "'{}' in parents and trashed=false".format(folder_id)}
@@ -148,20 +199,52 @@ def list_file(folder_id, drive):
         file_list))
     return [{"id": fld["id"], "title": fld["title"]} for fld in files]
 
+def validate_file_name(file_name):
+    if not file_name:
+        raise('no file provided')
+
+    folder, ext = file_name.split('.')[:-1][0], file_name.split('.')[-1]
+
+    if not folder:
+        raise('not a valid folder')
+
+    if ext not in ALLOWED_EXTENSIONS:
+        raise('%s not a valid file format' % ext)
+
+    return dict(file_name=file_name, folder=folder, ext=ext)
+
+def create_file(folder_id, file_name, data, ext, drive):
+    try:
+        ifile = drive.CreateFile(dict(
+            title=file_name,
+            parents=[dict(id=folder_id)],
+            mimeType=ALLOWED_EXTENSIONS[ext]))
+    except:
+        raise("Couldn't create %(file_name)s" % file_name)
+    ifile.SetContentString(df.to_csv(index=False))
+    ifile.Upload()
+    return ifile['alternateLink']
+
 @app.route('/gdrive/write', methods=['POST'])
 def write_file():
     """Given file.csv and json data, create file/file.csv.
+
     Make sure to set your .env for GDRIVE_PARENT_FOLDER_ID.
+
+    Args:
+
     """
     drive = init_auth()
-    args = request.json
-    data = args['data']
-    file_name = validate_file_name(args.get('file_name'))
-    folder = file_name['folder']
-    parent_folder = args.get('folder_id',
-                                  env.get('GDRIVE_PARENT_FOLDER_ID'))
-    file_name = file_name['file_name']
+    args = request.form
+    data = secure_filename(request.files['data'])
+    file_metadata = validate_file_name(args.get('file_name'))
+    folder = file_metadata['folder']
+    parent_folder = args.get(
+        'folder_id', env.get('GDRIVE_PARENT_FOLDER_ID')
+    )
 
+    file_name = file_metadata['file_name']
+    ext = file_metadata['ext']
     folder_list = list_folder(parent_folder, drive)
     match = list(filter(lambda x: x['title'] == folder, folder_list))
     folder_id = (
@@ -171,21 +254,19 @@ def write_file():
 
     file_list = list_file(folder_id, drive)
     match = list(filter(lambda x: x['title'] == file_name, file_list))
-    if not match:
-        url = create_file(
-            folder_id=folder_id,
-            file_name=file_name,
-            data=data,
-            drive=drive)
-    else:
+
+    if match:
         file_id = match[0]['id']
         to_delete = drive.CreateFile({"id": file_id})
         to_delete.Delete()
-        url = create_file(
-            folder_id=folder_id,
-            file_name=file_name,
-            data=data,
-            drive=drive)
+
+    url = create_file(
+        folder_id=folder_id,
+        file_name=file_name,
+        data=data,
+        ext=ext,
+        drive=drive)
+
     return jsonify(url)
 
 if __name__ == '__main__':
